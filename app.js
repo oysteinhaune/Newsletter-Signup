6545726e7572becbafa480513a5725e7d52cb856
Øystein Haugnes Aune <oysteinhaugnesaune@ysteins-MacBook-Pro.local>
2019-12-13 00:39:26 +0100

    add signin page, logo

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const request = require('request')
 
 const app = express()
 
+app.use('/css',express.static( 'public/css'));
+app.use('/images',express.static( 'public/images'));
+app.use('/',express.static( 'public/'));
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000')
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/signup.html')
 })
